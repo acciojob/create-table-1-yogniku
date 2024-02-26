18270ed942b1cc1e9adeb899976af72a2564f404
yogniku <156093318+yogniku@users.noreply.github.com>
2024-02-26 18:54:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,9 @@ function insert_Row() {
     //Write your code here
 	let table=document.getElementById("sampleTable")
    let btn = document.querySelector('input'); 
-	let row=table.insert_row();
-    row.insertCell(0).innerHTML=" New Cell1"
-	row.insertCell(1).innerHTML=" New Cell2"
-	
+	let row=table.insertRow();
+    row.insertCell(0).innerHTML="New Cell1"
+	row.insertCell(1).innerHTML="New Cell2"
 
-	
-  
   
 }
